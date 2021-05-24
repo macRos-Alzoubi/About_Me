fd1d5234bd0d7bda7026a178793cf1af34138235
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-05-24 23:25:56 +0300

    updating files

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
+"use strict";
+
 const printMessage = function (msg) {
   alert(msg);
 };
 
+const checkAnswer = function(answer){
+  answer = answer.toLowerCase();
+  if( answer === 'yes' || answer[0] === 'y'){
+    return true;
+  }
+  return false;
+}
+
 const rightAnswer = "You guess it right";
 const wrongAnswer = "You don't guess it right";
+let userAnswer = '';
 
 const userName = prompt("What is your name sir");
 alert(`Hell ${userName} you are very welcome in our website!`);
 
-const age = confirm("Am i 27 years old");
-age ? printMessage(rightAnswer) : printMessage(wrongAnswer);
+userAnswer = prompt("Am i 27 years old? (yes / no)");
+// if 'yes' console.log("You guess it right"), if 'no' console.log("You don't guess it right");
+checkAnswer(userAnswer) ? printMessage(rightAnswer) : printMessage(wrongAnswer);
 
-const major = confirm("Am i a Civil Engineer?");
-major ? printMessage(wrongAnswer) : printMessage(rightAnswer);
+userAnswer = prompt("Am i a Civil Engineer? (yes / no)");
+// if 'yes' console.log("You don't guess it right"), if 'no' console.log("You guess it right");
+checkAnswer(userAnswer) ? printMessage(wrongAnswer) : printMessage(rightAnswer);
 
-const favFood = confirm("Do i like Mansaf?");
-favFood ? printMessage(rightAnswer) : printMessage(wrongAnswer);
+userAnswer = prompt("Do i like Mansaf? (yes / no)");
+// if 'yes' console.log("You guess it right"), if 'no' console.log("You don't guess it right");
+checkAnswer(userAnswer) ? printMessage(rightAnswer) : printMessage(wrongAnswer);
 
-const tvShows = confirm("Do you think that i don't like TV-Shows?");
-tvShows ? printMessage(wrongAnswer) : printMessage(rightAnswer);
+userAnswer = prompt("Do you think that i don't like TV-Shows? (yes / no)");
+// if 'yes' console.log("You don't guess it right"), if 'no' console.log("You guess it right");
+checkAnswer(userAnswer) ? printMessage(wrongAnswer) : printMessage(rightAnswer);
 
-const molokhia = confirm("Do i like Molokhia?");
-molokhia ? printMessage(wrongAnswer) : printMessage(rightAnswer);
+userAnswer = prompt("Do i like Molokhia? (yes / no)");
+// if 'yes' console.log("You don't guess it right"), if 'no' console.log("You guess it right");
+checkAnswer(userAnswer) ? printMessage(wrongAnswer) : printMessage(rightAnswer);
 
 alert(`Thank you ${userName} for answering my questions!`);
